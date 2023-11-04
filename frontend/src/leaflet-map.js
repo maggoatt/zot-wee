f9b82cebe6b915cad249b33a51aafdd288a702c2
vicksey <vickyzhang@hotmail.ca>
2023-11-04 12:14:56 -0700

    Added map

---
+++ frontend/src/leaflet-map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([33.6460, -117.8427], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
