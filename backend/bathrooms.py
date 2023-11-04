a6c7b83d56a7d0bb0713c49ddc1d5a5e7907f769
Maggie Zhang <maggiegzhang@outlook.com>
2023-11-04 11:55:25 -0700

    updated bathroom information

--- backend/bathrooms.py
+++ backend/bathrooms.py
@@ -3,7 +3,7 @@ x = 'add'
 y= 'add'
 bathrooms = [["Aldrich Hall", 33.64854669998156, -117.84122589999998, [('B15', False), ('172', False)]],
 ["Anteater Recreation Center", 33.64359764777621, -117.82791864452422, [('104', True), ('105', True), ('272', True), ('275', True)]],
-['Arboretum', x, y, [('3', False)]]
+['Arboretum', 3.66430, -117.85339, [('3', False)]]
 ["Art, Culture, and Technology", 33.65099393675353, -117.8449827846549, [('1204', True)]],
 ["Art Studio", 33.650243641775035, -117.84494303069015, [('260A', True)]],
 ["Arts Annex", 33.647315512499304, -117.8468036153451, [('2000', True)]],
@@ -12,7 +12,7 @@ bathrooms = [["Aldrich Hall", 33.64854669998156, -117.84122589999998, [('B15', F
 ["Cross Cultural Center	", 33.64796140694652, -117.84183937327744, [('203', True)]],
 ["Croul Hall", 33.64386465195876, -117.84468073095017, [('B301', True)]],
 ["Engineering Tower", 33.6449489030145, -117.84109704629527, [('125', False)]],
-['Engineering', x, y, [('125', True), ('126', True)]]
+['Engineering & Computing Trailer', 33.644288728580754, -117.84020116091664, [('125', True), ('126', True)]]
 ["Gateway Study Center", 33.64756405097313, -117.84165041560507, [('2001', True), ('2002', True)]],
 ["Gavin Herbert Eye Institute", 33.641875719575665, -117.85219551560516, [('G005', True), ('1116', True), ('1137', True), ('1316', True)]],
 ["Hewitt Hall", 33.64349284800469, -117.8518024021139, True],
@@ -27,9 +27,11 @@ bathrooms = [["Aldrich Hall", 33.64854669998156, -117.84122589999998, [('B15', F
 ["Rowland Hall", 33.64476107186335, -117.84422225978636, True],
 ["Schneiderman Lecture Hall", 33.645798537622944, -117.8447573039678, True],
 ["Social & Behavioral Sciences Gateway", 33.64758536453718, -117.83906630211386, True],
-["Social Science Plaza B", 33.6472138340985, -117.83912613095008, True],
-["Steinhaus Hall", 33.64641757311412, -117.84489568862256, True],
-["Student Center", 33.64909423653421, -117.84223294730018, True],
+["Social Science Plaza B", 33.6472138340985, -117.83912613095008, [('1230', True)]],
+["Steinhaus Hall", 33.64641757311412, -117.84489568862256, [('115', True), ('118', True), ('262', True), ('264', True), ('364', True), ('368', True), ('468', True), ('470', True)]],
+["Student Center", 33.64909423653421, -117.84223294730018, [('G213', True), ('G215', True), ('G319', True), ('Starbucks', True)]],
+["Student Services",33.64802899261571, -117.84232542032353, [('105', True), ('106', True), ('240', True), ('250', True)]]
+['Studio Four', 33.650287097091734, -117.84573165093234, [('108', False), ('110', False)]]
 ["Humanities Gateway", 33.648251992362475, -117.84443361129894, False],
 ["Langson Library", 33.647368202328465, -117.84111867327755, False],
 ["Social Science Tower", 33.646779818239835, -117.8401352886227, False],
